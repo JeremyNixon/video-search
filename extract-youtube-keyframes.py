a1e16036710d5796f5cb780830fb6e77b0e83970
Jeremy Nixon <jnixon2@gmail.com>
2024-05-19 00:05:43 +0000

    ok this works great

--- extract-youtube-keyframes.py
+++ extract-youtube-keyframes.py
@@ -12,9 +12,9 @@ def download_keyframes(video_id, output_folder="keyframes", target_keyframes=60)
     def extract_keyframes(video_path, duration, interval, video_id):
         video_specific_folder = os.path.join(output_folder, video_id)
         os.makedirs(video_specific_folder, exist_ok=True)
-        for i in range(0, duration, interval):
+        for index, i in enumerate(range(0, duration, interval)):
             keyframe_filename = os.path.join(
-                video_specific_folder, f"keyframe_{i}.jpg")
+                video_specific_folder, f"keyframe_{index}_{i}.jpg")
             subprocess.run(['ffmpeg', '-ss', str(i), '-i',
                            video_path, '-frames:v', '1', keyframe_filename])
 
