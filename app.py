db7423937dd07c7b3c5ea7c3ebbd1d3b5eb80aa6
Jeremy Nixon <jnixon2@gmail.com>
2024-05-24 20:49:49 +0000

    small fix to get video-search-aditya folder to map to input path correctly

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import re
 from flask import Flask, render_template, request
 import os
 from random import random
@@ -14,19 +15,23 @@ app = Flask(__name__)
 clip_model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
 processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
 
+
 def weighted_rand(spec):
     i, sum = 0, 0
     r = random.random()
     for i in spec:
         sum += spec[i]
-        if r <= sum: return int(i)
+        if r <= sum:
+            return int(i)
+
 
 @app.route('/query-search', methods=['GET'])
 def query_search():
     query = request.args.get('query')
     query_embedding = embed_text(query)
     distances, indexes = search(query_embedding, 100)
-    similar_image_paths = [index_to_path[idx].split('static/')[1] for idx in indexes.flatten()]
+    similar_image_paths = [index_to_path[idx].split(
+        'static/')[1] for idx in indexes.flatten()]
     return render_template('similar_images.html', images=similar_image_paths, weighted_rand=weighted_rand)
 
 
@@ -34,26 +39,35 @@ def load_data(file_path):
     with open(file_path, 'rb') as f:
         index, np_embeddings, path_to_index, index_to_path = pickle.load(f)
     return index, np_embeddings, path_to_index, index_to_path
-faiss_index, np_embeddings, path_to_index, index_to_path = load_data('data3.pkl')
+
+
+faiss_index, np_embeddings, path_to_index, index_to_path = load_data(
+    'data3.pkl')
+
 
 def find_images_in_folder(folder_path, image_extensions=('*.jpg', '*.jpeg', '*.png', '*.gif')):
     image_files = []
     for root, _, files in os.walk(folder_path):
         for ext in image_extensions:
             for filename in fnmatch.filter(files, ext):
-                full_filepath = os.path.join(root, filename).split('static/')[1]
+                full_filepath = os.path.join(
+                    root, filename).split('static/')[1]
                 image_files.append((full_filepath, filename))
     return image_files
 
 # Define the search function to find the k nearest neighbors
+
+
 def search(embedding, k=5):
     # Ensure the input embedding has the correct data type
     embedding = np.array(embedding).astype('float32').reshape(1, -1)
     return faiss_index.search(embedding, k)
 
-def find_nearest_paths(input_path, k=100):    
+
+def find_nearest_paths(input_path, k=100):
     # Get the embedding for the input path
     input_path = os.path.join(os.getcwd(), 'static/') + input_path
+    input_path = re.sub(r'video-search-[^/]+', 'video-search', input_path)
     print(input_path)
     input_embedding = np_embeddings[path_to_index[input_path]]
     print(input_embedding)
@@ -64,10 +78,13 @@ def find_nearest_paths(input_path, k=100):
     nearest_paths = [index_to_path[idx] for idx in indexes.flatten()]
     return [p.split('static/')[1] for p in nearest_paths]
 
+
 def embed_text(text):
-    inputs = processor(text=text, return_tensors="pt", padding=True, truncation=True)
+    inputs = processor(text=text, return_tensors="pt",
+                       padding=True, truncation=True)
     with torch.no_grad():
         return clip_model.get_text_features(**inputs).numpy().tolist()
+
 
 @app.route('/similar-images')
 def similar_images():
@@ -76,11 +93,13 @@ def similar_images():
     similar_image_paths = find_nearest_paths(image_path)
     return render_template('similar_images.html', images=similar_image_paths, weighted_rand=weighted_rand)
 
+
 @app.route('/nearest_images')
 def nearest_images():
     input_path = request.args.get('path')
     nearest_image_paths = find_nearest_paths(input_path)
     return render_template('index.html', images=nearest_image_paths)
+
 
 def get_random_images(folder_path, num_images=5):
     all_images = find_images_in_folder(folder_path)
@@ -88,6 +107,7 @@ def get_random_images(folder_path, num_images=5):
     num_images = min(num_images, len(all_images))
     random_images = np.random.choice(all_images, num_images, replace=False)
     return [img.split('static/')[1] for img in random_images]
+
 
 def find_images_in_folder(folder_path, image_extensions=('*.jpg', '*.jpeg', '*.png', '*.gif')):
     image_files = []
@@ -105,10 +125,10 @@ def find_images_in_folder(folder_path, image_extensions=('*.jpg', '*.jpeg', '*.p
 
 @app.route('/')
 def index():
-    random_image_paths = get_random_images(os.path.join(os.getcwd(), 'static/youtube'), num_images=100)
+    random_image_paths = get_random_images(os.path.join(
+        os.getcwd(), 'static/youtube'), num_images=100)
     return render_template('similar_images.html', images=random_image_paths, weighted_rand=weighted_rand)
 
 
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=707)
